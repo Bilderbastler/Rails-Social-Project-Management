657a11c2462c0980e49cf8ff33298dddb6e815b5
Franziska Neumeister <info@franziska-neumeister.de>
2013-05-22 18:26:54 +0200

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SocialProject::Application.config.secret_token = '75e1b421ef1d112b103dbb25f269050c7adfa7df3159176a70079eb555c7f89f9bdd0f4f666a3532e025b1939cacc4ec27b54112a95d6a33221e2318a8fcb1c2'
